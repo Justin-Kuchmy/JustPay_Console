c422fc2e3d8e57fc8a86852db6a72dbf73935f26
Justin Kuchmy <Jukuchmy@gmail.com>
2025-09-22 11:00:54 +0800

    implemented parser.hpp class and ctor. added structs for menu and options

---
+++ parser.hpp
@@ -0,0 +1,69 @@
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
+#include <optional>   
+#include <unordered_map>
+#include <vector>
+using json = nlohmann::json;
+
+struct Option {
+    std::string label;
+    std::optional<std::string> submenu;
+    std::optional<std::string> action;  
+};
+
+struct Menu {
+    std::string title;
+    std::vector<Option> options;
+};
+
+class parser
+{
+    public:
+        parser();
+        ~parser();
+        std::unordered_map<std::string, Menu> menuMap;
+};
+
+parser::parser()
+{
+    std::ifstream file("menu.json");
+    if(!file.is_open())
+    {
+        std::cerr << "Error: could not open menu.json\n";
+        return;
+    }
+    json j;
+    file >> j;
+
+    //std::cout << j["menus"].items() << std::endl;
+    for (auto& [menuKey, menuVal] : j["menus"].items()) 
+    {
+        Menu m;
+        m.title = menuVal["title"].get<std::string>();
+        std::cout << m.title << std::endl;
+
+        for (auto& optVal : menuVal["options"]) 
+        {
+            Option opt;
+            opt.label = optVal["label"].get<std::string>();
+
+            if (optVal.contains("submenu")) {
+                opt.submenu = optVal["submenu"].get<std::string>();
+            }
+            if (optVal.contains("action")) {
+                opt.action = optVal["action"].get<std::string>();
+            }
+
+            m.options.push_back(opt);
+        }
+
+        menuMap[menuKey] = m;
+    }
+};
+
+
+parser::~parser()
+{
+
+};
