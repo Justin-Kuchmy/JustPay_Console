0a5585ce58f557fec33161a057b11063427bd81b
Justin Kuchmy <Jukuchmy@gmail.com>
2025-09-25 15:44:25 +0800

    added mod header and declarations and file cleanup

--- include/MenuManager.hpp
+++ include/MenuManager.hpp
@@ -2,23 +2,43 @@
 #define MENU_MANAGER_HPP
 
 #include <iostream>
-#include <ostream>
 #include <stack>
 #include <string>
+
 #include "Parser.hpp"
 #include "ActionDispatcher.hpp"
+#include "./modules/AdminSettingsModule.hpp"
+#include "./modules/AttendanceTrackingModule.hpp"
+#include "./modules/EmployeeManagementModule.hpp"
+#include "./modules/PayrollComputationModule.hpp"
+#include "./modules/PayslipGenerationModule.hpp"
+#include "./modules/ReportsLogsModule.hpp"
 
 class MenuManager {
-    Parser& p;
-    ActionDispatcher& dispatcher;
+    Parser p;
+    ActionDispatcher dispatcher;
+    AdminSettingsModule admin{dispatcher};
+    UserRoleManager roles{dispatcher};
+    ContributionTable tables{dispatcher};
+    AttendanceTrackingModule tracker{dispatcher};
+    EmployeeManagementModule empManagement{dispatcher};
+    LoanLedger ledger{dispatcher};
+    EmployeeAttendance empAttendance{dispatcher};
+    PayrollComputationModule payrollComp{dispatcher};
+    PayrollCalculator payrollCalc{dispatcher};
+    PayslipGenerationModule payslipGeneration{dispatcher};
+    PayslipGenerator payslipGenerator{dispatcher};
+    ReportsLogsModule reportlogs{dispatcher};
+    GovernmentReports governmentReports{dispatcher};
+    YearEndBenefitsCalculator benefitsCalc{dispatcher};
     std::stack<std::string> menuStack;
     Menu* currMenu;
-    friend std::ostream& operator<<(std::ostream& os, std::stack<std::string> stack);
-    friend std::ostream& operator<<(std::ostream& os, Option option);
-    friend std::ostream& operator<<(std::ostream& os, Menu menu);
+    friend std::ostream& operator<<(std::ostream& os, std::stack<std::string>& stack);
+    friend std::ostream& operator<<(std::ostream& os, Option& option);
+    friend std::ostream& operator<<(std::ostream& os, Menu& menu);
 
     public:
-        MenuManager(Parser& Parser, ActionDispatcher& disp);
+        MenuManager();
         ~MenuManager();
         void run();
 
@@ -77,7 +97,7 @@ inline std::ostream& operator<<(std::ostream& os, Menu menu)
     
 }
 
-inline MenuManager::MenuManager(Parser& Parser, ActionDispatcher& disp): p(Parser), dispatcher(disp) 
+inline MenuManager::MenuManager()
 {
     //std::cout << "___________MenuManager  Created___________" << std::endl;
     menuStack.push("main");
@@ -89,8 +109,8 @@ inline MenuManager::~MenuManager()
 std::cout << "___________MenuManager  Destroyed___________" << std::endl;
 };
 inline void MenuManager::run() {
-    int menuSize = currMenu->options.size();
     while (!menuStack.empty()) {
+        int menuSize = currMenu->options.size();
         displayMenu();
         int choice = getUserChoice(menuSize);
         handleChoice(choice);
@@ -99,7 +119,6 @@ inline void MenuManager::run() {
 inline void MenuManager::displayMenu()
 {
     clearScreen();
-    std::cout << "Stack: [" << menuStack << "]" << std::endl;
     std::cout << currMenu->title << std::endl;
     for (size_t i = 0; i < p.menuMap[menuStack.top()].options.size(); i++)
     {
@@ -125,27 +144,35 @@ inline int MenuManager::getUserChoice(int maxOption)
 };
 inline void MenuManager::handleChoice(int choice)
 {
-    Option chosen = currMenu->options[choice-1];
-    if(chosen.submenu.has_value())
+    if (choice > 0 && choice <= currMenu->options.size()) 
     {
-        if(menuStack.top() != chosen.submenu.value())
-            menuStack.push(chosen.submenu.value());
-        currMenu = &p.menuMap[menuStack.top()];
-    }
-    else if (chosen.action.has_value())
-    {
-        if (chosen.action == "back" || chosen.action == "exit") 
+        Option chosen = currMenu->options[choice-1];
+        if(chosen.submenu.has_value())
         {
-            menuStack.pop(); // same behavior
+            if(menuStack.top() != chosen.submenu.value())
+            menuStack.push(chosen.submenu.value());
             currMenu = &p.menuMap[menuStack.top()];
         }
-        else 
+        else if (chosen.action.has_value())
         {
-            int delim = menuStack.top().find_first_of("_");
-            dispatcher.runAction(menuStack.top().substr(0, delim), chosen.action.value()); 
-            std::cout << "\nPress Enter to continue...";
-            std::cin.ignore();
+            if (chosen.action == "back" || chosen.action == "exit") 
+            {
+                menuStack.pop(); 
+                currMenu = &p.menuMap[menuStack.top()];
+            }
+            else 
+            {
+                int delim = menuStack.top().find_first_of("_");
+                dispatcher.runAction(menuStack.top().substr(0, delim), chosen.action.value()); 
+                std::cout << "\nPress Enter to continue...";
+                std::cin.ignore();
+            }
         }
+    } 
+    else 
+    {
+        std::cout << "Invalid choice. Please try again.\n";
     }
 };
-
+    
+    
