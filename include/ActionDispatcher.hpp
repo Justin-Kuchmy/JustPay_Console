3e1bb7f60f1828dc6a9ed4e9c4a5c2f1cc4fb0f3
Justin Kuchmy <Jukuchmy@gmail.com>
2025-09-25 15:02:59 +0800

    moved to separate file

--- include/ActionDispatcher.hpp
+++ include/ActionDispatcher.hpp
@@ -14,20 +14,3 @@ public:
 private:
     std::unordered_map<std::string, std::unordered_map<std::string, std::function<void()>>> actions;
 };
-
-ActionDispatcher::ActionDispatcher(){};
-ActionDispatcher::~ActionDispatcher(){};
-
-void ActionDispatcher::regAction(const std::string& group,const std::string& action,std::function<void()> func)
-{
-    actions[group][action] = std::move(func);
-}
-
-void ActionDispatcher::runAction(const std::string& group, const std::string& action)
-{
-    if (actions.contains(group) && actions[group].contains(action)) {
-        actions[group][action]();
-    } else {
-        std::cout << "Unknown action: " << group << "::" << action << std::endl;
-    }
-}
---
+++ src/ActionDispatcher.cpp
@@ -0,0 +1,18 @@
+#include "ActionDispatcher.hpp"
+
+ActionDispatcher::ActionDispatcher(){};
+ActionDispatcher::~ActionDispatcher(){};
+
+void ActionDispatcher::regAction(const std::string& group,const std::string& action,std::function<void()> func)
+{
+    actions[group][action] = std::move(func);
+}
+
+void ActionDispatcher::runAction(const std::string& group, const std::string& action)
+{
+    if (actions.contains(group) && actions[group].contains(action)) {
+        actions[group][action]();
+    } else {
+        std::cout << "Unknown action: " << group << "::" << action << std::endl;
+    }
+}
