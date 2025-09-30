50ff8c9587e5d1e50d5efce5b3f9cf2fc7a43558
Justin Kuchmy <Jukuchmy@gmail.com>
2025-09-30 11:17:21 +0800

    implemented Base Repo

--- Repos/Headers/BaseRepository.hpp
+++ Repos/Headers/BaseRepository.hpp
@@ -1,9 +1,25 @@
 #ifndef BASEREPO_HPP
 #define BASEREPO_HPP
+
 #include <sqlite3.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <optional>
 
 class BaseRepository {
-
+protected:
+    sqlite3* db = nullptr;
+    
+public:
+    explicit BaseRepository(const std::string& dbName);
+    virtual ~BaseRepository();
+    virtual std::string getCreateTableSQL() const = 0;
+    bool createTable();
+    
+protected:
+    bool execute(const std::string& sql);
 };
+
 
 #endif
