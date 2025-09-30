40a6186cf5c66d3d448b25de485a85946562a243
Justin Kuchmy <Jukuchmy@gmail.com>
2025-09-30 11:08:01 +0800

    new file added

---
+++ Repos/Headers/BaseRepository.hpp
@@ -0,0 +1,9 @@
+#ifndef BASEREPO_HPP
+#define BASEREPO_HPP
+#include <sqlite3.h>
+
+class BaseRepository {
+
+};
+
+#endif
---
+++ Repos/Headers/EmployeeRepository.hpp
@@ -0,0 +1,10 @@
+#ifndef EMPLOYEEREPO_HPP
+#define EMPLOYEEREPO_HPP
+#include "BaseRepository.hpp"
+#include <sqlite3.h>
+
+class EmployeeRepository: public BaseRepository {
+
+};
+
+#endif
