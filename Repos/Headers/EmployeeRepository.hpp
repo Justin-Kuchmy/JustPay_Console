2a45fcade19d9e9c2799558b41d86225fc057ae5
Justin Kuchmy <Jukuchmy@gmail.com>
2025-09-30 11:20:17 +0800

    Declared Employee Repo

--- Repos/Headers/EmployeeRepository.hpp
+++ Repos/Headers/EmployeeRepository.hpp
@@ -1,10 +1,28 @@
 #ifndef EMPLOYEEREPO_HPP
 #define EMPLOYEEREPO_HPP
 #include "BaseRepository.hpp"
+#include "../../Entities/DataObjects.hpp"
 #include <sqlite3.h>
 
 class EmployeeRepository: public BaseRepository {
+    public: 
+        explicit EmployeeRepository();
+        
+        //CREATE
+        std::string getCreateTableSQL() const override;
+        bool insertEmployee(const Employee& Employee);
 
+        // READ
+        std::optional<Employee> getById(int id);             // Find one by ID
+        std::vector<Employee> getAll();                      // Return all
+        std::vector<Employee> findByName(const std::string& name); // Simple search
+
+        // UPDATE
+        bool updateEmployee(const Employee& emp); // Update all fields by ID
+
+        // DELETE
+        bool deleteEmployee(int id);          // Delete by ID
+        bool deleteAll();                   // Truncate table
 };
 
 #endif
