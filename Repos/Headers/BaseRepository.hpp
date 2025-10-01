2f105f1e06f729ed110cebe3ab2cd66ffeb7c0c6
Justin Kuchmy <Jukuchmy@gmail.com>
2025-10-01 16:11:54 +0800

    impl BaseRepo

--- Repos/Headers/BaseRepository.hpp
+++ Repos/Headers/BaseRepository.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include <optional>
+#include <functional>
 
 class BaseRepository {
 protected:
@@ -19,6 +20,26 @@ public:
     
 protected:
     bool execute(const std::string& sql);
+    template <typename T>
+    std::vector<T> query(const std::string& sql,std::function<T(sqlite3_stmt*)> mapper)
+    {
+        std::vector<T> results;
+        sqlite3_stmt* stmt; /* OUT: Statement handle */
+
+        
+        //database, sql_statement, max_length, out_statement, ptr to unused part of sql string
+        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
+            std::cerr << "Failed to prepare: " << sqlite3_errmsg(db) << std::endl;
+            return results;
+        }
+
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            results.push_back(mapper(stmt));
+        }
+
+        sqlite3_finalize(stmt);
+        return results;
+    }
 };
 
 
