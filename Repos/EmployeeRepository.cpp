4f86e85e80546d99e0ada4f43dca63d16bdfef30
Justin Kuchmy <Jukuchmy@gmail.com>
2025-10-01 17:00:24 +0800

    fixed sql for table creation

--- Repos/EmployeeRepository.cpp
+++ Repos/EmployeeRepository.cpp
@@ -9,7 +9,15 @@
         std::string EmployeeRepository::getCreateTableSQL()const
         {
             return R"(
-                CREATE TABLE IF NOT EXISTS employees (
+
+                PRAGMA foreign_keys = OFF;
+
+                DROP TABLE IF EXISTS emergency_contacts;
+                DROP TABLE IF EXISTS dependents;
+                DROP TABLE IF EXISTS employees;
+
+
+                CREATE TABLE IF NOT EXISTS "employees" (
                     employeeId TEXT PRIMARY KEY,
                     fullName TEXT NOT NULL,
                     department INTEGER NOT NULL,
@@ -37,7 +45,7 @@
                     FOREIGN KEY (employeeId) REFERENCES employees(employeeId)
                 );
 
-                CREATE TABLE IF NOT EXISTS dependents (
+                CREATE TABLE IF NOT EXISTS "dependents" (
                     dependentId INTEGER PRIMARY KEY AUTOINCREMENT,
                     employeeId TEXT NOT NULL,
                     name TEXT NOT NULL,
@@ -45,6 +53,22 @@
                     birthday TEXT NOT NULL,
                     FOREIGN KEY (employeeId) REFERENCES employees(employeeId)
                 );
+
+                INSERT INTO "employees" VALUES
+                ('01-0001','Alice Santos',0,'HR Manager',2,0,'2022-01-15',NULL,'12-3456789-0','987654321','123456789',45000.0,5000.0,'alice@example.com',1),
+                ('02-0002','Bob Reyes',1,'Finance Associate',0,1,'2023-03-01',NULL,'23-9876543-1','123456789','987654321',28000.0,2000.0,'bob@example.com',1),
+                ('02-0003','Mary Mabulay',1,'Finance',0,0,'2016-01-15',NULL,'11-3159781-0','387254961','148563279',80000.0,5000.0,'mary@example.com',1);
+
+                INSERT INTO "emergency_contacts" (employeeId, name, relation, address, contactNo) VALUES
+                ('01-0001','Juan Santos','Brother','Quezon City','09171234567'),
+                ('02-0002','Maria Reyes','Mother','Makati','09987654321'),
+                ('02-0003','Justin Kuchmy','Husband','CDO','09455554567');
+
+                INSERT INTO "dependents" (employeeId, name, relation, birthday) VALUES
+                ('01-0001','Juan Santos','Brother','1991-05-27'),
+                ('02-0002','Maria Reyes','Mother','1962-01-12'),
+                ('02-0003','Justin Kuchmy','Husband','1991-12-31');
+
             )";
 
         };
