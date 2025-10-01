76a411134a2464a16ee759aa028c1b59b3df7a40
Justin Kuchmy <Jukuchmy@gmail.com>
2025-10-01 16:02:45 +0800

    init emp repo and service

--- Repos/EmployeeRepository.cpp
+++ Repos/EmployeeRepository.cpp
@@ -1,1 +1,51 @@
 #include "./Headers/EmployeeRepository.hpp"
+
+
+        EmployeeRepository::EmployeeRepository() : BaseRepository("payroll.db")
+        {
+            EmployeeRepository::execute(this->getCreateTableSQL());
+        }
+
+        //CREATE
+        std::string EmployeeRepository::getCreateTableSQL()const
+        {
+
+        };
+
+        bool EmployeeRepository::insertEmployee(const Employee& Employee)
+        {
+
+        };
+
+        // READ
+        std::optional<Employee> EmployeeRepository::getById(int id)
+        {
+
+        };// Find one by ID
+
+        std::vector<Employee> EmployeeRepository::getAll()
+        {
+
+        };// Return all
+
+        std::vector<Employee> EmployeeRepository::findByName(const std::string& name)
+        {
+
+        }; // Simple search
+
+        // UPDATE
+        bool EmployeeRepository::updateEmployee(const Employee& emp)
+        {
+
+        }; // Update all fields by ID
+
+        // DELETE
+        bool EmployeeRepository::deleteEmployee(int id)
+        {
+
+        };// Delete by ID
+
+        bool EmployeeRepository::deleteAll()
+        {
+
+        };// Truncate table
