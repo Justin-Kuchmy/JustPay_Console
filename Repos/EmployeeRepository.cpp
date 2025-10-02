cf853e5536bd7bc97a38bbc40a751353dc171fef
Justin Kuchmy <Jukuchmy@gmail.com>
2025-10-02 13:47:26 +0800

    Implemented getById

--- Repos/EmployeeRepository.cpp
+++ Repos/EmployeeRepository.cpp
@@ -1,4 +1,5 @@
 #include "./Headers/EmployeeRepository.hpp"
+#include <format>
 
 
         EmployeeRepository::EmployeeRepository() : BaseRepository("payroll.db")
@@ -72,37 +73,96 @@
             )";
 
         };
-
-        bool EmployeeRepository::insertEmployee(const Employee& Employee)
+        bool EmployeeRepository::insertEmployee(const Employee& employee)
         {
-            return false;
-        };
+            std::string sql = std::format(
+                "INSERT INTO employees VALUES ('{}', '{}', {}, '{}', {}, {}, '{}', '{}', '{}', '{}', '{}', {}, {}, '{}', {})",
+                employee.employeeId,
+                employee.fullName,
+                to_int(employee.department),       // enum -> int
+                employee.position,
+                to_int(employee.jobLevel),         // enum -> int
+                to_int(employee.status),           // enum -> int
+                to_string(employee.dateHired),     // Date -> string (YYYY-MM-DD)
+                to_string(employee.dateSeparation),
+                employee.sssNumber,
+                employee.philHealthNumber,
+                employee.hdmfNumber,
+                employee.monthlyBasicSalary,
+                employee.monthlyAllowances,
+                employee.personalEmail,
+                employee.isActive
+            );
+
+            return execute(sql);
+        }
 
         // READ
-        std::optional<Employee> EmployeeRepository::getById(int id)
+        std::optional<Employee> EmployeeRepository::getById(std::string id)
         {
-            return std::nullopt;
+            std::string sql = std::format("select * from employees where employeeId = '{}';", id);
+            auto results = EmployeeRepository::query<Employee>(
+            sql,
+            [](sqlite3_stmt* stmt) -> Employee {
+                Employee e;
+                e.employeeId = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+                e.fullName =   reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+                e.department = to_department(sqlite3_column_int(stmt, 2));  
+                e.position = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
+                e.jobLevel =  to_jobLevel(sqlite3_column_int(stmt, 4));  
+                e.status =     to_status(sqlite3_column_int(stmt, 5));     
+                e.dateHired =    from_string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 6)));   
+
+                const unsigned char* text = sqlite3_column_text(stmt, 7);
+                if (text) {
+                    e.dateSeparation = from_string(reinterpret_cast<const char*>(text));
+                } else {
+                    e.dateSeparation = Date{1900,1,1};
+                }
+                e.sssNumber = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 8));
+                e.philHealthNumber = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 9));
+                e.hdmfNumber = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 10));
+                e.monthlyBasicSalary = sqlite3_column_double(stmt, 11);
+                e.monthlyAllowances =  sqlite3_column_double(stmt, 12);
+                e.personalEmail = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 13));
+                e.isActive =  sqlite3_column_int(stmt, 14);
+                return e;
+            });
+
+            if(!results.empty())
+            {
+                return results.front();
+            }
+            else
+            {
+                std::cout << "failed to get anything from the db: ";
+                return std::nullopt;
+            }
         };// Find one by ID
 
         std::vector<Employee> EmployeeRepository::getAll()
         {
+            std::string sql = std::format("select * from employees;");
             return {};
         };// Return all
 
         std::vector<Employee> EmployeeRepository::findByName(const std::string& name)
         {
+            std::string sql = std::format("select * from employeee where fullName = '{}';", name);
             return {};
         }; // Simple search
 
         // UPDATE
         bool EmployeeRepository::updateEmployee(const Employee& emp)
         {
+            std::string sql = std::format("SELECT * from employeee where employeeId = '{}'", emp.employeeId);
             return false;
         }; // Update all fields by ID
 
         // DELETE
-        bool EmployeeRepository::deleteEmployee(int id)
+        bool EmployeeRepository::deleteEmployee(std::string id)
         {
+            std::string sql = std::format("update employees set isActive = false where employeeId = '{}'", id);
             return false;
         };// Delete by ID
 
