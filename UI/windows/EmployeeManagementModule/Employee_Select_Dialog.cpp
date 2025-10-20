58cb01f73d45d1a6440f10ce17cc8c8335a71dfa
Justin Kuchmy <Jukuchmy@gmail.com>
2025-10-20 14:45:50 +0800

    updated

--- UI/windows/EmployeeManagementModule/Employee_Select_Dialog.cpp
+++ UI/windows/EmployeeManagementModule/Employee_Select_Dialog.cpp
@@ -1,17 +1,50 @@
-#include "../../../Include/UI/Components/ui_employee_select_dialog.h"
-#include "../../../Include/UI/Components/Employee_Select_Dialog.h"
+#include "../../../Include/UI/Components/Headers/ui_employee_select_dialog.h"
+#include "../../../Include/UI/Components/Headers/Employee_Select_Dialog.h"
 #include <QWidget>
+#include <QListWidgetItem>
 
-EmployeeSelectDialog::EmployeeSelectDialog(QWidget *parent)
+EmployeeSelectDialog::EmployeeSelectDialog(QWidget *parent): QDialog(parent), ui(new Ui::EmployeeSelectDialog)
 {
-    
+    ui->setupUi(this); 
+    connect(ui->employeeList, &QListWidget::itemClicked,
+            this, &EmployeeSelectDialog::onEmployeeSelected);
 }
+
+void EmployeeSelectDialog::populateEmployeeList() {
+   ui->employeeList->clear();
+
+    if (!empList) return;
+    for (const auto& emp : *empList) { 
+        ui->employeeList->addItem(QString::fromStdString(emp.fullName));
+    }
+}
+
+EmployeeSelectDialog::~EmployeeSelectDialog()
+{
+    delete ui;
+}
+
 QString EmployeeSelectDialog::getSelectedEmployee() const
 {
-    QString nStr("");
-    return nStr;
+    return selectedEmployee;
 }
 void EmployeeSelectDialog::on_buttonBox_accepted()
 {
-    
+     if (ui->employeeList->currentItem())
+        selectedEmployee = ui->employeeList->currentItem()->text();
+}
+
+void EmployeeSelectDialog::onEmployeeSelected(QListWidgetItem* item)
+{
+    if (!item || !empList) return;
+
+    QString selectedName = item->text();
+
+    for (const auto& emp : *empList) {
+        if (QString::fromStdString(emp.fullName) == selectedName) {
+            selectedEmployee = selectedName;
+            qDebug() << "Selected employee:" << selectedEmployee;
+            break;
+        }
+    }
 }
