c795530a94cf07d0d3903e9fe0171482b8802787
Justin Kuchmy <Jukuchmy@gmail.com>
2025-10-23 13:09:57 +0800

    correction

--- Source/Utils/DialogFactory.cpp
+++ Source/Utils/DialogFactory.cpp
@@ -1,6 +1,8 @@
 
 #include "Utils/DialogFactory.h"
 #include "UI/Add_Employee_Dialog.h"
+#include "UI/Add_Dependent_Dialog.h"
+#include "UI/Add_Emergency_Contact_Dialog.h"
 
 
 std::unordered_map<QString, DialogFactory::Creator> DialogFactory::registry;
@@ -9,9 +11,8 @@ void DialogFactory::registerDialogs()
 {
     registry.clear();
     registry["add_employee"] = []() { return new AddEmployeeDialog(); };
-    registry["add_contact"] = []() { return new AddEmergencyContactDialog(); };
+    registry["add_contact"] = []() { return new EmergencyContactDialog(); };
     registry["add_dependent"] = []() { return new AddDependentDialog(); };
-    //registry.insert("add_employee", []() { return new AddEmployeeDialog(); });
     
 };
 
