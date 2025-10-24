25a015eddd38ba927843077400747ed2b689aa02
Justin Kuchmy <Jukuchmy@gmail.com>
2025-10-24 15:20:40 +0800

    added connect impl onOkClicked

--- UI/Windows/Add_Dependent_Dialog.cpp
+++ UI/Windows/Add_Dependent_Dialog.cpp
@@ -4,6 +4,8 @@
 AddDependentDialog::AddDependentDialog(QWidget *parent): QDialog(parent), ui(new Ui::AddDependentDialog)
 {
     ui->setupUi(this); 
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, this, &AddDependentDialog::onOKClicked);
+    connect(ui->buttonBox, &QDialogButtonBox::rejected, this, &AddDependentDialog::onCancelClicked);
 }
 
 AddDependentDialog::~AddDependentDialog()
@@ -13,10 +15,20 @@ AddDependentDialog::~AddDependentDialog()
 
 void AddDependentDialog::onOKClicked()
 {
-
+    m_Dependent.name = ui->nameLineEdit->text().toStdString();
+    m_Dependent.relation = ui->relationLineEdit->text().toStdString();
+    m_Dependent.birthday = from_string(ui->bdayDateEdit->date().toString("yyyy-MM-dd").toStdString());
+    qDebug() << m_Dependent;
+    accept();
 };
 
 void AddDependentDialog::onCancelClicked()
 {
-
+    reject();
 };
+
+
+Dependent AddDependentDialog::getDependentData() const
+{
+    return this->m_Dependent;
+}
